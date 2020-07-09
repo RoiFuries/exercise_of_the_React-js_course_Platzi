7dca9209f86184abc2bc23bf352353e4f2d45e72
RoiFuries <roi.furies@gmail.com>
2020-07-09 11:20:20 -0500

    inicie el proyecto, instale babel, reacte hice mi primer componente

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Helloworld = () =>{
+    <h1>Homa mundo</h1>
+}
+export default Helloworld;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDrom from 'react-dom';
+import HellowWorld from './components/HelloWorld.jsx'
+
+ReactDrom.render(<HellowWorld/>, document.getElementById(app));
